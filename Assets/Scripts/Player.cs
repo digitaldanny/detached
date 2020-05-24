918fa14ee6e772a74d4c346f214f10962795b0ca
sweatpantsdanny <d.hamilton@ufl.edu>
2020-05-24 00:07:54 -0400

    leveraged project settings from previous project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    // Configs
+    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpPower = 5f;
+
+    // State
+    bool isAlive = true;
+
+    // Cached component references
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
+    Collider2D myCollider2D;
+    float gravityScaleAtStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myCollider2D = GetComponent<Collider2D>();
+        gravityScaleAtStart = myRigidBody.gravityScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+        Jump();
+        FlipSprite();
+    }
+
+    private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // left to right = -1 to +1
+
+        // hardcoding y to 0 would make jump buggy
+        Vector2 playerVelocity = new Vector2(controlThrow*runSpeed, myRigidBody.velocity.y); 
+
+        myRigidBody.velocity = playerVelocity;
+
+        // Tell animator when to play run animator
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", playerHasHorizontalSpeed);
+    }
+
+    private void Jump()
+    {
+        // only allow jumping if touching the ground
+        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground")))
+            return;
+
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpPower);
+            myRigidBody.velocity += jumpVelocityToAdd;
+        }
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+
+        // if player is moving horizontally
+        if (playerHasHorizontalSpeed)
+        {
+            // reverse the current scaling of the x axis
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
+    }
+}
